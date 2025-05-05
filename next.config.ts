9141776005e116c3f7e94379440b6dd64a36d3ff
Nicoleta Mocanu <nicoleta.mocanu87@gmail.com>
2025-05-04 21:37:34 -0700

    Test hook now clean

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const                   nextConfig: NextConfig = {
   /* config options here */
 };
 
-jhjkkh,j
 
 export default nextConfig;
